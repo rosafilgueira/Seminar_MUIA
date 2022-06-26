42a0e922feb00b056163a6e3d4f7ce0be225cf0b
Rosa Filgueira <rosa.filgueira@ed.ac.uk>
2022-06-26 15:43:18 +0100

    .

--- Spark_Applications/spark_submit_SparkPi.sh
+++ Spark_Applications/spark_submit_SparkPi.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-$HOME/spark-2.4.0-bin-hadoop2.7/bin/spark-submit --verbose --class org.apache.spark.examples.SparkPi  --master spark://$1:7077 --deploy-mode cluster --supervise $HOME/spark-2.4.0-bin-hadoop2.7/examples/jars/spark-examples_2.11-2.4.0.jar  10
+
+SPARK_HOME="/opt/homebrew/Cellar/apache-spark/3.2.1/libexec/"
+EXAMPLES="examples/jars/spark-examples_2.12-3.2.1.jar"
+
+spark-submit --verbose --class org.apache.spark.examples.SparkPi \
+             --master local[*] \
+              $SPARK_HOME$EXAMPLES  500
 
